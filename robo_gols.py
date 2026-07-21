543d92add60d30cf3651102e42fdcb7639de5601
Raulen02 <raulenluizz@gmail.com>
2026-07-21 11:53:42 -0300

    Update message format for Telegram bot

--- robo_gols.py
+++ robo_gols.py
@@ -4,8 +4,8 @@ token = "8924431376:AAHtUD9kI_gQRTTFSzRSZvtii8uX9cM-qF4"
 chat_id = "519222308"
 url = f"https://api.telegram.org/bot{token}/sendMessage"
 
-mensagem = "🚨 *Robô de Gols
-Conectado com Sucesso no Render!*"
-payload = {"chat_id": chat_id, "text": mensagem, "parse_mode": "Markdown"}
+mensagem = "Robo Conectado com Sucesso no Render!"
+payload = {"chat_id": chat_id, "text": mensagem}
+
 resposta = requests.post(url, json=payload)
-print("Status:", resposta.json())
+print(resposta.json())
