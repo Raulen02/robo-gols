fd0e7b0ddac29e28852399f95430ea748e078577
Raulen02 <raulenluizz@gmail.com>
2026-07-21 12:02:14 -0300

    Update message and add loop for continuous running

--- robo_gols.py
+++ robo_gols.py
@@ -1,11 +1,15 @@
+import time
 import requests
 
 token = "8924431376:AAHtUD9kI_gQRTTFSzRSZvtii8uX9cM-qF4"
 chat_id = "519222308"
 url = f"https://api.telegram.org/bot{token}/sendMessage"
 
-mensagem = "Robo Conectado com Sucesso no Render!"
-payload = {"chat_id": chat_id, "text": mensagem}
+# Mensagem inicial de aviso
+payload = {"chat_id": chat_id, "text": "Robo de Gols Ativo e Monitorando!"}
+requests.post(url, json=payload)
 
-resposta = requests.post(url, json=payload)
-print(resposta.json())
+# Mantém o aplicativo rodando para o Render não desligar
+while True:
+    print("Robo rodando...")
+    time.sleep(60)
